7a0b5dd851869e4e0a245795624ebaa29612ef7f
Bhumitpatel1492 <patelbhumit484@gmail.com>
2025-12-11 22:09:59 +0530

    [First comment]

---
+++ android/app/.cxx/Debug/19m1a6b1/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/ReactAndroid/ReactAndroidConfig.cmake
@@ -0,0 +1,27 @@
+if(NOT TARGET ReactAndroid::hermestooling)
+add_library(ReactAndroid::hermestooling SHARED IMPORTED)
+set_target_properties(ReactAndroid::hermestooling PROPERTIES
+    IMPORTED_LOCATION "/Users/bhumit/.gradle/caches/8.13/transforms/c2436c90d4d2043f04002c89057d81a5/transformed/react-android-0.82.1-debug/prefab/modules/hermestooling/libs/android.arm64-v8a/libhermestooling.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/bhumit/.gradle/caches/8.13/transforms/c2436c90d4d2043f04002c89057d81a5/transformed/react-android-0.82.1-debug/prefab/modules/hermestooling/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
+if(NOT TARGET ReactAndroid::jsi)
+add_library(ReactAndroid::jsi SHARED IMPORTED)
+set_target_properties(ReactAndroid::jsi PROPERTIES
+    IMPORTED_LOCATION "/Users/bhumit/.gradle/caches/8.13/transforms/c2436c90d4d2043f04002c89057d81a5/transformed/react-android-0.82.1-debug/prefab/modules/jsi/libs/android.arm64-v8a/libjsi.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/bhumit/.gradle/caches/8.13/transforms/c2436c90d4d2043f04002c89057d81a5/transformed/react-android-0.82.1-debug/prefab/modules/jsi/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
+if(NOT TARGET ReactAndroid::reactnative)
+add_library(ReactAndroid::reactnative SHARED IMPORTED)
+set_target_properties(ReactAndroid::reactnative PROPERTIES
+    IMPORTED_LOCATION "/Users/bhumit/.gradle/caches/8.13/transforms/c2436c90d4d2043f04002c89057d81a5/transformed/react-android-0.82.1-debug/prefab/modules/reactnative/libs/android.arm64-v8a/libreactnative.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/bhumit/.gradle/caches/8.13/transforms/c2436c90d4d2043f04002c89057d81a5/transformed/react-android-0.82.1-debug/prefab/modules/reactnative/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
---
+++ android/app/.cxx/Debug/19m1a6b1/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/fbjni/fbjniConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET fbjni::fbjni)
+add_library(fbjni::fbjni SHARED IMPORTED)
+set_target_properties(fbjni::fbjni PROPERTIES
+    IMPORTED_LOCATION "/Users/bhumit/.gradle/caches/8.13/transforms/f8516065e14310a96719b7cadc84df53/transformed/fbjni-0.7.0/prefab/modules/fbjni/libs/android.x86_64/libfbjni.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/bhumit/.gradle/caches/8.13/transforms/f8516065e14310a96719b7cadc84df53/transformed/fbjni-0.7.0/prefab/modules/fbjni/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
