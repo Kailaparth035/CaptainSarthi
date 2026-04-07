3d5f0db7e756342470f2ff645d426cc4e060fb34
Bhumitpatel1492 <patelbhumit484@gmail.com>
2026-04-07 22:01:10 +0530

    [Add] New build configuration files and logs for arm64-v8a architecture. Introduced hash_key.txt, additional_project_files.txt, and various CMake-related files to support the build process, enhancing project structure and build management.

---
+++ android/app/.cxx/Debug/19m1a6b1/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/hermes-engine/hermes-engineConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET hermes-engine::hermesvm)
+add_library(hermes-engine::hermesvm SHARED IMPORTED)
+set_target_properties(hermes-engine::hermesvm PROPERTIES
+    IMPORTED_LOCATION "/Users/bhumit/.gradle/caches/8.13/transforms/6060e53a772ef2a67199be576b1e5a44/transformed/hermes-android-0.82.1-debug/prefab/modules/hermesvm/libs/android.armeabi-v7a/libhermesvm.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/bhumit/.gradle/caches/8.13/transforms/6060e53a772ef2a67199be576b1e5a44/transformed/hermes-android-0.82.1-debug/prefab/modules/hermesvm/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
